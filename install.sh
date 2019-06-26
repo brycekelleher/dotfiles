cd432e27abd3e996a26f19f918760afa6495b598
brycek <bryce_kelleher@hotmail.com>
2019-06-25 17:26:44 -0700

    added install

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+# bash
+#ln -sf $PWD/.bashrc ~/.bashrc
+
+# tmux
+ln -sf $PWD/.tmux ~/.tmux.conf 
+
+# xbindkeys
+#ln -sf $PWD/xbindkeysrc ~/.xbindkeysrc
+
+# vim
+ln -sf $PWD/.vimrc ~/.vimrc
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
+
+# git
+ln -sf $PWD/.gitconfig ~/.gitconfig
+#ln -sf $PWD/gitignore ~/.gitignore
