adf20c7b49279d45db0f2a07ccce80a99f66276e
brycek <bryce_kelleher@hotmail.com>
2019-06-25 17:54:43 -0700

    fixed config file names

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ PWD=`pwd`
 #ln -sf $PWD/.bashrc ~/.bashrc
 
 # tmux
-ln -sf $PWD/.tmux ~/.tmux.conf 
+ln -sf $PWD/tmux ~/.tmux.conf 
 
 # xbindkeys
 #ln -sf $PWD/xbindkeysrc ~/.xbindkeysrc
 
 # vim
-ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/vimrc ~/.vimrc
 #git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #vim +PluginInstall +qall
 
 # git
-ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/gitconfig ~/.gitconfig
 #ln -sf $PWD/gitignore ~/.gitignore
